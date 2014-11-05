65acc0ec26aaf873cfb02844062d94452d33d39f
Sunny Cheung <sunnycyk@yahoo.com>
2014-11-05 16:05:15 +0800

    Initial Commit

---
+++ RBLRapiro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RBLRapiro
+//
+//  Created by Sunny Cheung on 5/11/14.
+//  Copyright (c) 2014 khl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
